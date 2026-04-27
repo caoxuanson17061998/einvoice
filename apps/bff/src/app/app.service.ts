42edcffd3b32da233478b81934e55967aad5106d
soncx <soncx@gmail.com>
2026-04-27 10:12:44 +0700

    feat/#bff: add commitlint configuration with husky hooks

---
+++ apps/bff/src/app/app.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { PORT } from '@common/constants/common.constant';
+
+@Injectable()
+export class AppService {
+  getData(): { message: string } {
+    return { message: `Hello API on port ${PORT}` };
+  }
+}
