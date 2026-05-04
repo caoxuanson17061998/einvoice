c0a232e1abcbe042a8e54326f974864f2c6e3f0e
Cao Xuan Son <caoxuanson17061998@gmail.com>
2026-05-04 13:52:43 +0700

    refactor/#: simplify eslint configuration by removing unused rules and parser

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,11 @@
 import nx from '@nx/eslint-plugin';
-import parser from 'jsonc-eslint-parser';
 
 export default [
   ...nx.configs['flat/base'],
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
   {
-    ignores: ['**/dist', '**/node_modules'],
+    ignores: ['**/dist'],
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
@@ -27,28 +26,17 @@ export default [
     },
   },
   {
-    files: ['**/*.ts', '**/*.tsx', '**/*.cts', '**/*.mts', '**/*.js', '**/*.jsx', '**/*.cjs', '**/*.mjs'],
+    files: [
+      '**/*.ts',
+      '**/*.tsx',
+      '**/*.cts',
+      '**/*.mts',
+      '**/*.js',
+      '**/*.jsx',
+      '**/*.cjs',
+      '**/*.mjs',
+    ],
     // Override or add rules here
     rules: {},
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    rules: {
-      '@typescript-eslint/no-unused-vars': 'error',
-    },
-  },
-  {
-    files: ['**/*.spec.ts', '**/*.spec.tsx', '**/*.spec.js', '**/*.spec.jsx'],
-    languageOptions: {},
-    rules: {},
-  },
-  {
-    files: ['**/*.json'],
-    languageOptions: {
-      parser: parser,
-    },
-    rules: {
-      '@nx/dependency-checks': 'error',
-    },
   },
 ];
